12856a3d4c14eec1869fdff6f69241ee5790d10b
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-06-09 17:13:48 +0530

    Hero section added and required ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
 
     </main>
   );
